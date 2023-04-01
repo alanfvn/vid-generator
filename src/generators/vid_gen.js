acf0b224a24fe14b383bfef6d0487c8c40d5ac20
alanfvn <alandavidg16@gmail.com>
2023-04-01 14:43:37 -0600

    Fixed generated video length

--- src/generators/vid_gen.js
+++ src/generators/vid_gen.js
@@ -1,22 +1,32 @@
-import Ffmpeg from "fluent-ffmpeg"
+import ffmpeg from "fluent-ffmpeg"
 import stream from 'stream'
 
-function ffmpeg_async(image, audio_file, output_dir){
+function getFileDuration(file){
+  return new Promise((resolve, reject)=>{
+    ffmpeg.ffprobe(file, (err, meta)=>{
+      if(err){
+        return reject(err)
+      }
+      return resolve(meta.format.duration)
+    })
+  })
+}
+
+
+function ffmpeg_async(image, audio_file, duration, output_dir){
   return new Promise((resolve, reject)=>{
     const imgStream = new stream.PassThrough()
     imgStream.write(image, 'utf8')
-
-    Ffmpeg()
-      .input(imgStream)
-      .loop()
+    ffmpeg()
       .input(audio_file)
+      .input(imgStream)
+      .loop(duration)
       .videoCodec('libx264')
       .outputOptions([
         '-preset medium',
         '-tune stillimage',
         '-crf 18',
         '-pix_fmt yuv420p',
-        // '-shortest'
       ])
       .on('err',(err)=>{
         console.log(err)
@@ -31,7 +41,8 @@ function ffmpeg_async(image, audio_file, output_dir){
 }
 
 async function generateVideo(image, audio_file, output_dir){
-  await ffmpeg_async(image, audio_file, output_dir)
+  const dur = await getFileDuration(audio_file)
+  await ffmpeg_async(image, audio_file, dur, output_dir)
 }
 
 export default generateVideo
